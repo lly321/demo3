e85197698afee83c8b75532cc74cf2f588f3a403
ly <1913959983@qq.com>
2019-12-14 16:18:12 +0800

    1.1

---
+++ src/main/java/com/example3/model/student.java
@@ -0,0 +1,22 @@
+package com.example3.model;
+
+public class student {
+    private Integer sid;
+    private String sname;
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+}
